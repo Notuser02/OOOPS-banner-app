decfcb5636ab3031b0f919c8e98b769b95b0b1af
sye <salla.siddhanth@gmail.com>
2026-02-16 23:52:04 +0530

    saving local file before UC2

---
+++ OOPSBannerApp.java
@@ -0,0 +1,23 @@
+public class OOPSBannerApp {
+    /**
+ * OOPSBannerApp UC2
+ * Prints OOPS in large banner using * and spaces
+ * @author Siddhanth
+ * @version 2.0
+ */
+
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+
+        System.out.println(" *****     *****     ******    ***** ");
+        System.out.println("*     *   *     *    *     *  *     *");
+        System.out.println("*     *   *     *    *     *  *      ");
+        System.out.println("*     *   *     *    ******    ***** ");
+        System.out.println("*     *   *     *    *              *");
+        System.out.println("*     *   *     *    *        *     *");
+        System.out.println(" *****     *****     *         ***** ");
+
+    }
+}
+
+}
