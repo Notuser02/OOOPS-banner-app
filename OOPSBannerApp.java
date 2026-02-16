589bd49d8b3608229536c862725cf4c0103232fa
sye <salla.siddhanth@gmail.com>
2026-02-16 22:49:14 +0530

    UC1: Print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+ * OOPSBannerApp
+ * This program prints "OOPS" to the console.
+ * Demonstrates basic Java program structure.
+ *
+ * @author Siddhanth
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
